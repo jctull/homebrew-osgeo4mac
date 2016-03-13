a20df923aee72b6a0fe2e31b39daf998979502ab
jctull <jctull@gmail.com>
2016-03-12 19:05:43 -0800

    Grass-71 cleanup for gdal2 integration

--- Formula/grass-71.rb
+++ Formula/grass-71.rb
@@ -33,7 +33,7 @@ class Grass71 < Formula
   depends_on "cairo"
   depends_on :x11  # needs to find at least X11/include/GL/gl.h
   depends_on "openblas" => :optional
-  depends_on "liblas" => :optional
+  depends_on "jctull/osgeo4mac/liblas" => :optional
   depends_on "netcdf" => :optional
   depends_on "ffmpeg" => :optional
 
